07cb2eee993e5311448deb4304d91a5eaa4d566d
Brayo-The-Great <111040819+Brayo-The-Great@users.noreply.github.com>
2022-10-06 20:57:23 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,31 +1,49 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
- * _realloc - reallocates a memory block.
- * @ptr: pointer to the memory previously allocated.
- * @old_size: size, in bytes, of the allocated space of ptr.
- * @new_size: new size, in bytes, of the new memory block.
+ * string_nconcat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: amount of bytes.
  *
- * Return: ptr.
- * if new_size == old_size, returns ptr without changes.
- * if malloc fails, returns NULL.
+ * Return: pointer to the allocated memory.
+ * if malloc fails, status value is equal to 98.
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
+	char *sout;
+	unsigned int ls1, ls2, lsout, i;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
+		;
+
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+		;
+
+	if (n > ls2)
+		n = ls2;
+
+	lsout = ls1 + n;
+
+	sout = malloc(lsout + 1);
+
+	if (sout == NULL)
 		return (NULL);
-	}
 
-	if (ptr == NULL)
-		ptr = malloc(new_size);
+	for (i = 0; i < lsout; i++)
+		if (i < ls1)
+			sout[i] = s1[i];
+		else
+			sout[i] = s2[i - ls1];
 
-	if (new_size == old_size)
-		return (ptr);
+	sout[i] = '\0';
 
-	free(ptr);
-	ptr = malloc(new_size);
-
-	return (ptr);
+	return (sout);
 }
